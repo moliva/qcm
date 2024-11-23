131eb3ed41246d3407a604b770191917197a39bd
moliva <oliva.miguelh@gmail.com>
2024-11-23 11:51:51 -0300

    refactor: use secure cookies

--- src/utils.ts
+++ src/utils.ts
@@ -41,26 +41,23 @@ export function decodeArgument(arg: string | undefined): string[] {
 }
 
 export function removeCookie(cname: string): void {
-  // document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`
-  localStorage.removeItem(cname)
+  document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`
 }
 
 export function getCookie(cname: string): string | null {
-  // let name = cname + '='
-  // let decodedCookie = decodeURIComponent(document.cookie)
-  // let ca = decodedCookie.split(';')
-  // for (let i = 0; i < ca.length; i++) {
-  //   let c = ca[i]
-  //   while (c.charAt(0) == ' ') {
-  //     c = c.substring(1)
-  //   }
-  //   if (c.indexOf(name) == 0) {
-  //     return c.substring(name.length, c.length)
-  //   }
-  // }
-  // return ''
-
-  return localStorage.getItem(cname)
+  let name = cname + '='
+  let decodedCookie = decodeURIComponent(document.cookie)
+  let ca = decodedCookie.split(';')
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i]
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return null
 }
 
 export function setCookie(name: string, value: string, expirationDays?: number): void {
@@ -68,14 +65,10 @@ export function setCookie(name: string, value: string, expirationDays?: number):
   if (expirationDays) {
     const date = new Date()
     date.setTime(date.getTime() + expirationDays * 24 * 60 * 60 * 1000) // millis to days
-    expirationString = date.toUTCString()
+    expirationString = `;expires=${date.toUTCString()}`
   }
 
-  // document.cookie = `${name}=${value};SameSite=Strict;Secure;expires=${date.toUTCString()};path=/`
-  // document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`
-  // document.cookie = `${name}=${value};path=/`
-
-  localStorage.setItem(name, value)
+  document.cookie = `${name}=${value};SameSite=Strict;Secure;path=/;${expirationString}`
 }
 
 export function parseIdToken(token: string): any {
