b2b5cd5c4202acc3ceae63857011735f24df23c0
moliva <oliva.miguelh@gmail.com>
2025-01-12 12:55:12 -0300

    chore(build): bundling options with terser, clean of deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -10,12 +10,34 @@ export default defineConfig({
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     // devtools(),
-    solidPlugin(),
+    solidPlugin()
   ],
   server: {
-    port: 3000,
+    port: 3000
   },
   build: {
     target: 'esnext',
-  },
-});
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.trace']
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['solid-js']
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    },
+    sourcemap: false,
+    // Improve build performance
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 1000
+  }
+})
